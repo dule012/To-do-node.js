bd3778062ef18ceebc08f505b127e2370586145a
BIT student <dusanmihajlovic012@gmail.com>
2018-10-23 02:29:49 +0200

    mongodb dependencies

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
+app.listen(3001, () => {
+    console.log('server listening')
+})
